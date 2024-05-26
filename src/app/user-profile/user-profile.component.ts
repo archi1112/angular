6d2e41fcb12886dfccb512a64e7d0010da83c232
agolia <goliaarchi@gmail.com>
2024-05-26 15:14:59 +0530

    events and 2-way data binding

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
@@ -11,5 +12,13 @@ export class UserProfileComponent {
   name = "sanjay";
   age=28
   salary=45000
+defaultInputVal:any="45"
+
+// function for event on input attribute
+  onChange(e : Event){
+    const value=(e.target as HTMLInputElement).value
+    //    topic-3  updating value back from input attribute
+    this.defaultInputVal=value
+  }
   
 }
